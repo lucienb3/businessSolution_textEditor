0770f78b79fa048f920abbfa35dd8fab6a111b4d
lucienb3 <lucienb3@nycstudents.net>
2026-01-12 10:11:35 -0500

    Update Lesson8.py

--- Lesson8.py
+++ Lesson8.py
@@ -2,20 +2,48 @@ from tkinter import *
 #need to install on all machines
 from tkmacosx import Button
 
+import random
+
+
 root = Tk()
 root.title("Updating Labels")
 
 #Set size of window
 #root.geometry("300x200")
 
-button1 = Button(root, text='Button1!')
-button2 = Button(root, text='Button2!')
-button3 = Button(root, text='Button3!')
-rndbutton = Button(root, text='Random Button!')
+
+
+
+
+def update1():
+	label1.config(text = 'Updated 1!')
+
+def update2():
+	label2.config(text = 'Updated 2!')
+
+def update3():
+	label3.config(text = 'Updated 3!')
+
+
+def random1():
+	num = random.randint(1, 6)
+	print(num)
+
+
+
+
+
+
+button1 = Button(root, text='Button1!', command=update1)
+button2 = Button(root, text='Button2!', command=update2)
+button3 = Button(root, text='Button3!', command=update3)
+rndbutton = Button(root, text='Random Button!', command=random1)
 
 label1 = Label(root, text='Label 1')
 label2 = Label(root, text='Label 2')
 label3 = Label(root, text='Label 3')
+
+
 
 
 
